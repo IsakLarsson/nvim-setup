e6760e30dd6f316e58903e715db45bcd69be2b71
Isak Larsson <isaklarsson98@hotmail.com>
2023-01-25 09:33:21 +0100

    Darker background

--- lua/larsson/core/colorscheme.lua
+++ lua/larsson/core/colorscheme.lua
@@ -3,6 +3,7 @@ vim.g.nightflyCursorColor = true
 vim.g.nightflyWinSeparator = 2
 
 vim.g.gruvbox_flat_style = "hard"
+vim.g.gruvbox_colors = { bg = "black" }
 vim.o.background = "dark"
 -- local dark = "#1d2021"
 --[[ require("gruvbox").setup({
